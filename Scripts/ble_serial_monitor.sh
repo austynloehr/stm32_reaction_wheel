3a78a6161f2ad97aa52fd7a2e8c12b1801f5c107
Austyn Loehr <austynloehr@outlook.com>
2023-11-18 15:43:04 -0500

    Added docs for new motor and controller. Added some logic to control initialization angle and gyro reset angle.
    Added STM32_Reaction_Wheel_Architecture.drawio.png
    
    Added STM32_Reaction_Wheel_Architecture-IO.drawio.png
    
    Added STM32_Reaction_Wheel_Architecture-IO.drawio.png
    
    Update STM32_Reaction_Wheel_Architecture.drawio
    
    Update STM32_Reaction_Wheel_Architecture.drawio

--- Scripts/ble_serial_monitor.sh
+++ Scripts/ble_serial_monitor.sh
@@ -2,7 +2,7 @@
 # After the HC05 module is paired, this script binds the device to rfcomm0 and opens a serial connection 
 
 # HC-05 Mac Address
-macAddr='00:14:03:05:01:70'
+macAddr='38:AB:41:3C:D1:31'
 
 # HC-05 UART baud rate
 baudRate=921600
